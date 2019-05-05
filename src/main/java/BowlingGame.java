8498966a3ef49ed0e48a4a03aa7778d3107a1cc5
anupriya johari <anupriya.johari@wipro.com>
2019-05-05 23:32:24 +0100

    modularising the code

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -13,17 +13,33 @@ public class BowlingGame {
         int index = 0;
         for(int frame = 0 ; frame < 10; frame++){
             if(isSpare(index)){
-              score += 10 + rolls[index+2];
+              score += 10 + getStrikeBonus(index, 2);
               index +=2;
-            }else if(rolls[index] == 10){
-                score += 10 + rolls[index + 1] + rolls[index + 2];
+            }else if(isStrike(rolls[index])){
+                score += 10 + getSpareBonus(index);
                 index++;
             } else {
-                score += rolls[index] + rolls[index + 1];
+                score += getPinsInFrame(index, index + 1);
                 index += 2;
             }
         }
         return score;
+    }
+
+    private int getPinsInFrame(int index, int i) {
+        return rolls[index] + rolls[i];
+    }
+
+    private int getStrikeBonus(int index, int i) {
+        return rolls[index + i];
+    }
+
+    private int getSpareBonus(int index) {
+        return rolls[index + 1] + rolls[index + 2];
+    }
+
+    private boolean isStrike(int roll) {
+        return roll == 10;
     }
 
     private boolean isSpare(int index) {
