2ad583716a02a4941edb236ce43e6d0ae2866212
anupriya johari <anupriya.johari@wipro.com>
2019-05-05 22:30:52 +0100

    adding first test for gutter game

---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,8 @@
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+
+    public void testGutterGame () throws Exception {
+        BowlingGame bowlingGame = new BowlingGame();
+    }
+}
