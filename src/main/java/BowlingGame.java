0352bb87d6d0483544cb7af15533de44c57da320
anupriya johari <anupriya.johari@wipro.com>
2019-05-05 23:13:28 +0100

    code refractored to handle spare

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,16 +1,20 @@
 public class BowlingGame {
 
-    private int score = 0;
     private int rolls[] = new int [21];
     private int count =0;
 
     public void roll(int pins) {
-        score += pins;
         rolls[count] = pins;
         count++;
     }
 
     public int score() {
+        int score = 0;
+        int i = 0;
+        for(int frame = 0 ; frame < 10; frame++){
+            score += rolls[i] + rolls[i+1];
+            i +=2;
+        }
         return score;
     }
 }
