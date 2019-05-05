8a2fbfe8fac5d17055d2139ee99b76f32385b535
anupriya johari <anupriya.johari@wipro.com>
2019-05-05 22:49:55 +0100

    case for all 1 sorted

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,24 +1,31 @@
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest extends TestCase {
 
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setUp(){
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
     public void testGutterGame () throws Exception {
-        BowlingGame bowlingGame = new BowlingGame();
-        for(int i = 0 ;i< 20 ;i++){
+
+        for(int i = 0 ;i < 20 ;i++){
             bowlingGame.roll(0);
-            assertEquals(0, bowlingGame.score());
         }
+        assertEquals(0, bowlingGame.score());
     }
 
     @Test
     public void testAll1Game () throws Exception {
-        BowlingGame bowlingGame = new BowlingGame();
-        for(int i = 0 ;i< 20 ;i++){
-            bowlingGame.roll(0);
-            assertEquals(20, bowlingGame.score());
+        for(int i = 0 ;i < 20 ;i++){
+            bowlingGame.roll(1);
         }
+        assertEquals(20, bowlingGame.score());
     }
 
 }
