82c25ce85cb3685705745688d82e41056837bf3c
anupriya johari <anupriya.johari@wipro.com>
2019-05-05 22:41:44 +0100

    adding all 1 game test

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,2 +1,8 @@
 public class BowlingGame {
+    public void roll(int pins) {
+    }
+
+    public int score() {
+        return 0;
+    }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,8 +1,24 @@
 import junit.framework.TestCase;
+import org.junit.Test;
 
 public class BowlingGameTest extends TestCase {
 
+    @Test
     public void testGutterGame () throws Exception {
         BowlingGame bowlingGame = new BowlingGame();
+        for(int i = 0 ;i< 20 ;i++){
+            bowlingGame.roll(0);
+            assertEquals(0, bowlingGame.score());
+        }
     }
+
+    @Test
+    public void testAll1Game () throws Exception {
+        BowlingGame bowlingGame = new BowlingGame();
+        for(int i = 0 ;i< 20 ;i++){
+            bowlingGame.roll(0);
+            assertEquals(20, bowlingGame.score());
+        }
+    }
+
 }
