f312c5078e4fbb642f761c87754b1aa203045fc9
anupriya johari <anupriya.johari@wipro.com>
2019-05-05 23:00:51 +0100

    adding code for keeping memory of all rolls

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,9 +1,13 @@
 public class BowlingGame {
 
     private int score = 0;
+    private int rolls[] = new int [21];
+    private int count =0;
 
     public void roll(int pins) {
         score += pins;
+        rolls[count] = pins;
+        count++;
     }
 
     public int score() {
